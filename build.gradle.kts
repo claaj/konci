8dd4c5ad43110cd895fc7faa66cfe7fa83439818
claaj <matias.cajal@hotmail.com>
2023-10-05 16:50:19 -0300

    Task para jar con todo incluido

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,3 +66,20 @@ tasks.test {
     useJUnitPlatform()
 }
 
+val mainClass = "ui.MainKt"
+
+tasks {
+    register("Jar", Jar::class.java) {
+        //archiveClassifier.set("all")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest {
+            attributes("Main-Class" to mainClass)
+        }
+        from(configurations.runtimeClasspath.get()
+            .onEach { println("add from dependencies: ${it.name}") }
+            .map { if (it.isDirectory) it else zipTree(it) })
+        val sourcesMain = sourceSets.main.get()
+        sourcesMain.allSource.forEach { println("add from sources: ${it.name}") }
+        from(sourcesMain.output)
+    }
+}
