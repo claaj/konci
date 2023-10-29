a7345af1c31ae5346ca849b26d17eda7d81febde
claaj <matias.cajal@hotmail.com>
2023-10-29 16:37:11 -0300

    Mejor lógica de dialogos al procesar

--- src/main/kotlin/ui/conciliar/tabs/procesar/Dialogos.kt
+++ src/main/kotlin/ui/conciliar/tabs/procesar/Dialogos.kt
@@ -1,108 +1,106 @@
-@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
-
 package ui.conciliar.tabs.procesar
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Composable
-internal fun dialogoAviso(
-    cerrarAlerta: () -> Unit,
-    icon: ImageVector,
-    titulo: String,
-    descripcion: String,
-    textoBoton: String,
-) {
-    AlertDialog(
-        onDismissRequest = { cerrarAlerta() },
-    ) {
-        Surface(
-            modifier = Modifier.wrapContentHeight().wrapContentWidth(),
-            shape = MaterialTheme.shapes.extraLarge,
-            tonalElevation = AlertDialogDefaults.TonalElevation
-        ) {
-            Column(Modifier.padding(15.dp)) {
-                Icon(
-                    icon,
-                    contentDescription = null,
-                    Modifier.align(Alignment.CenterHorizontally).size(40.dp)
-                )
-
-                Text(
-                    titulo,
-                    fontSize = 20.sp,
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    fontWeight = FontWeight.Bold
-                )
-
-                Text(
-                    descripcion,
-                    fontSize = 14.sp
-                )
-
-                TextButton(
-                    onClick = { cerrarAlerta() },
-                    modifier = Modifier.align(Alignment.End)
-                ) {
-                    Text(textoBoton)
-                }
-            }
-        }
-    }
+enum class Dialogo {
+    Aviso,
+    Error,
+    Exito,
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun dialogoError(
+internal fun dialogo(
+    tipo: Dialogo,
     cerrarAlerta: () -> Unit,
-    titulo: String,
     descripcion: String,
-    textoBoton: String,
+) {
 
-    ) {
+    val colorSurface: Color
+    val colorLetra: Color
+    val icono: ImageVector
+    val titulo: String
+
+    when (tipo) {
+        Dialogo.Aviso -> {
+            colorSurface = MaterialTheme.colorScheme.primaryContainer
+            colorLetra = MaterialTheme.colorScheme.onPrimaryContainer
+            icono = Icons.Filled.Warning
+            titulo = "Proceso no realizado"
+        }
+
+        Dialogo.Error -> {
+            colorSurface = MaterialTheme.colorScheme.errorContainer
+            colorLetra = MaterialTheme.colorScheme.onErrorContainer
+            icono = Icons.Filled.Error
+            titulo = "Fallo en el procesamiento"
+        }
+
+        Dialogo.Exito -> {
+            colorSurface = MaterialTheme.colorScheme.primaryContainer
+            colorLetra = MaterialTheme.colorScheme.onPrimaryContainer
+            icono = Icons.Filled.Done
+            titulo = "Proceso ejecutado con éxito"
+        }
+    }
+
     AlertDialog(
         onDismissRequest = { cerrarAlerta() },
     ) {
         Surface(
-            modifier = Modifier.wrapContentHeight().wrapContentWidth(),
+            modifier = Modifier.wrapContentHeight().wrapContentWidth().width(512.dp).height(256.dp),
             shape = MaterialTheme.shapes.extraLarge,
             tonalElevation = AlertDialogDefaults.TonalElevation,
-            color = MaterialTheme.colorScheme.errorContainer
+            color = colorSurface,
         ) {
-            Column(Modifier.padding(15.dp)) {
-                Icon(
-                    Icons.Filled.Error,
-                    contentDescription = null,
-                    Modifier.align(Alignment.CenterHorizontally).size(40.dp)
-                )
+            Column(modifier = Modifier.padding(16.dp)) {
+                Column(verticalArrangement = Arrangement.Center) {
+                    Icon(
+                        icono,
+                        contentDescription = null,
+                        Modifier.align(Alignment.CenterHorizontally).size(48.dp)
+                    )
 
-                Text(
-                    titulo,
-                    fontSize = 20.sp,
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
-                )
+                    Spacer(Modifier.fillMaxWidth().height(8.dp))
 
-                Text(
-                    descripcion,
-                    fontSize = 14.sp
-                )
+                    Text(
+                        titulo,
+                        fontSize = 20.sp,
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
 
-                TextButton(
-                    onClick = { cerrarAlerta() },
-                    modifier = Modifier.align(Alignment.End)
+                    Spacer(Modifier.fillMaxWidth().height(4.dp))
+
+                    Text(
+                        descripcion,
+                        fontSize = 14.sp
+                    )
+                }
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.BottomEnd
                 ) {
-                    Text(textoBoton, color = MaterialTheme.colorScheme.onErrorContainer)
+                    TextButton(
+                        onClick = { cerrarAlerta() },
+                    ) {
+                        Text("Entendido", color = colorLetra)
+                    }
                 }
             }
         }
--- src/main/kotlin/ui/conciliar/tabs/procesar/TabProcesar.kt
+++ src/main/kotlin/ui/conciliar/tabs/procesar/TabProcesar.kt
@@ -2,9 +2,6 @@ package ui.conciliar.tabs.procesar
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -26,17 +23,17 @@ fun TabProcesar(
     setupExternos: (List<Path>) -> DataFrame<*>,
     setupLocales: (List<Path>) -> DataFrame<*>
 ) {
-    var dialogoAlerta by remember { mutableStateOf(false) }
-    var dialogoArchivoNoEncontrado by remember { mutableStateOf(false) }
-    var dialogoFormatoInvalido by remember { mutableStateOf(false) }
-    var dialogoTodoOk by remember { mutableStateOf(false) }
+    var mostrarDialogo by remember { mutableStateOf(false) }
+    var descripcionDialogo by remember { mutableStateOf("") }
+    var tipoDialogo by remember { mutableStateOf(Dialogo.Aviso) }
 
-    val elegirArchivo = JFileChooser().apply {
+    val elegirCarpeta = JFileChooser().apply {
         fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
         dialogTitle = "Seleccione una carpeta de guardado"
         approveButtonText = "Seleccionar"
         approveButtonToolTipText = "Seleccionar la carpeta actual como la ruta de guardado"
     }
+
     Scaffold(
         floatingActionButton = {
             ExtendedFloatingActionButton(
@@ -49,10 +46,13 @@ fun TabProcesar(
                         archivosLocales = estado.listaLocal.toList(),
                         setupExternos = setupExternos,
                         setupLocales = setupLocales,
-                        dialogoFaltanDatos = { dialogoAlerta = true },
-                        dialogoArchivoNoEncotrado = { dialogoArchivoNoEncontrado = true },
-                        dialogoFormatoInvalido = { dialogoFormatoInvalido = true },
-                        dialogoTodoOk = { dialogoTodoOk = true },
+                        origenExterno = estado.origenExterno,
+                        origenLocal = estado.origenLocal,
+                        dialogo = { descripcion, tipo ->
+                            descripcionDialogo = descripcion
+                            tipoDialogo = tipo
+                            mostrarDialogo = true
+                        },
                     )
                 },
                 containerColor = MaterialTheme.colorScheme.primary
@@ -63,14 +63,14 @@ fun TabProcesar(
         floatingActionButtonPosition = FabPosition.End
     ) {
         Surface {
-            Column(modifier = Modifier.padding(10.dp).fillMaxSize()) {
+            Column(modifier = Modifier.padding(8.dp).fillMaxSize()) {
                 Text("Carpeta guardado")
-                Row(Modifier.padding(top = 10.dp, bottom = 10.dp)) {
+                Row(Modifier.padding(top = 8.dp, bottom = 8.dp)) {
                     FilledTonalButton(
                         onClick = {
-                            elegirArchivo.showOpenDialog(null)
-                            if (elegirArchivo.selectedFile != null) {
-                                estado.rutaGuardado.value = elegirArchivo.selectedFile.toString()
+                            elegirCarpeta.showOpenDialog(null)
+                            if (elegirCarpeta.selectedFile != null) {
+                                estado.rutaGuardado.value = elegirCarpeta.selectedFile.toString()
                             }
                         }
                     ) {
@@ -78,55 +78,25 @@ fun TabProcesar(
                     }
 
                     Surface(
-                        modifier = Modifier.fillMaxWidth().padding(top = 5.dp, start = 15.dp)
+                        modifier = Modifier.fillMaxWidth().padding(top = 4.dp, start = 16.dp)
                             .clip(RoundedCornerShape(25)).height(38.dp),
                     ) {
                         Text(
-                            text = estado.rutaGuardado.value,
+                            text = estado.rutaGuardado.value.orEmpty(),
                             textAlign = TextAlign.Start,
                             overflow = TextOverflow.Ellipsis,
                             maxLines = 1,
                             fontSize = 14.sp,
-                            modifier = Modifier.align(Alignment.CenterVertically).padding(start = 10.dp, top = 5.dp)
+                            modifier = Modifier.align(Alignment.CenterVertically).padding(start = 8.dp, top = 4.dp)
                         )
                     }
                 }
             }
-            if (dialogoAlerta) {
-                dialogoAviso(
-                    cerrarAlerta = { dialogoAlerta = false },
-                    icon = Icons.Filled.Warning,
-                    titulo = "Proceso no realizado",
-                    descripcion = "Esto se debe a la falta de archivos para procesar y/o de una carpeta de guardado.",
-                    "Entendido"
-                )
-            }
-
-            if (dialogoArchivoNoEncontrado) {
-                dialogoError(
-                    cerrarAlerta = { dialogoArchivoNoEncontrado = false },
-                    titulo = "Error al procesar las planillas",
-                    descripcion = "Algunas de las planillas ya no se encuentran en la ruta indicada, o la ruta de guardado ya no existe.",
-                    textoBoton = "Entendido"
-                )
-            }
-
-            if (dialogoFormatoInvalido) {
-                dialogoError(
-                    cerrarAlerta = { dialogoFormatoInvalido = false },
-                    titulo = "Error al procesar las planillas",
-                    descripcion = "Alguna(s) de la(s) planilla(s) poseen un formato inválido para la operación e impuesto seleccionados.",
-                    textoBoton = "Entendido"
-                )
-            }
-
-            if (dialogoTodoOk) {
-                dialogoAviso(
-                    cerrarAlerta = { dialogoTodoOk = false },
-                    icon = Icons.Filled.Done,
-                    titulo = "Proceso realizado con éxito",
-                    descripcion = "Todas las planillas fueron procesadas y guardadas correctamente.",
-                    textoBoton = "Entendido",
+            if (mostrarDialogo) {
+                dialogo(
+                    tipo = tipoDialogo,
+                    cerrarAlerta = { mostrarDialogo = false },
+                    descripcion = descripcionDialogo
                 )
             }
         }
