721758f489bdef96cf98190da6f4224ab8e4f24a
claaj <matias.cajal@hotmail.com>
2023-10-31 15:04:36 -0300

    Añadir función `conciliar`

---
+++ src/main/kotlin/com/github/claaj/konci/planillas/proceso/Conciliar.kt
@@ -0,0 +1,42 @@
+package com.github.claaj.konci.planillas.proceso
+
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.concat
+import org.jetbrains.kotlinx.dataframe.api.count
+import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
+import org.jetbrains.kotlinx.dataframe.api.head
+import java.nio.file.Path
+import kotlin.math.absoluteValue
+
+val COLUMNAS = listOf("CUIT", "RAZON_SOC", "N_COMP", "FECHA", "IMPORTE")
+
+fun conciliar(
+    archivosExternos: List<Path>,
+    archivosLocales: List<Path>,
+    setupExternos: (List<Path>) -> DataFrame<*>,
+    setupLocales: (List<Path>) -> DataFrame<*>
+): DataFrame<*> {
+    val dfExterno = setupExternos(archivosExternos)
+    val dfLocal = setupLocales(archivosLocales)
+    var dfFiltrado = dataFrameOf(COLUMNAS, listOf())
+
+    val cuitsUnicos = cuitsUnicos(dfExterno, dfLocal)
+
+    for (cuit in cuitsUnicos) {
+        if (!mismoImporteTotal(dfExterno, dfLocal, cuit)) {
+            for (importe in importesPorCuitUnicosTotal(dfExterno, dfLocal, cuit)) {
+                val filtroAfip = filtroCuitImporte(dfExterno, cuit, importe)
+                val filtroTango = filtroCuitImporte(dfLocal, cuit, importe)
+
+                val diferenciaCantidad = filtroAfip.count() - filtroTango.count()
+                if (diferenciaCantidad > 0) {
+                    dfFiltrado = dfFiltrado.concat(filtroAfip.head(diferenciaCantidad.absoluteValue))
+                }
+                if (diferenciaCantidad < 0) {
+                    dfFiltrado = dfFiltrado.concat(filtroTango.head(diferenciaCantidad.absoluteValue))
+                }
+            }
+        }
+    }
+    return dfFiltrado
+}
